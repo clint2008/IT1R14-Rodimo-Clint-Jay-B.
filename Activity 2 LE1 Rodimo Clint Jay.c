8a0470a7ec82b195eaf534e109330d59c5882c7b
clint2008 <rodimoclintjay@gmail.com>
2026-08-17 23:17:17 +0800

    Add welcome screen in Activity 2 LE1
    This code displays a welcome screen for C programming.

---
+++ Activity 2 LE1 Rodimo Clint Jay.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Welcome Screen
+    printf("****************************************\n");
+    printf("*                                      *\n");
+    printf("*        WELCOME TO C PROGRAMMING      *\n");
+    printf("*                                      *\n");
+    printf("*          LEARN | CREATE | CODE       *\n");
+    printf("*                                      *\n");
+    printf("*       YOUR/OUR CODING JOURNEY STARTS *\n");
+    printf("*                HERE!                 *\n");
+    printf("*                                      *\n");
+    printf("****************************************\n");
+
+    return 0;

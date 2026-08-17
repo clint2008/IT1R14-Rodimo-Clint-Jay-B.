6989248f8f65a27e75e654a548e7a14e358dce60
clint2008 <rodimoclintjay@gmail.com>
2026-08-17 23:15:26 +0800

    Add student profile display in new C file

---
+++ LE1 Rodimo Clint Jay.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Student Profile
+        printf("========================================\n");
+            printf("             STUDENT PROFILE\n");
+                printf("========================================\n");
+
+                    // Student Information
+                        printf("Name: Clint Jay B. Rodimo\n");
+                            printf("Course and Section: BSIT-1R14\n");
+                                printf("Year Level: 1st Year\n");
+
+                                    printf("========================================\n");
+                                        printf("             FUTURE PROGRAMMER\n");
+                                            printf("========================================\n");
+
+                                                return 0;
+                                                }
